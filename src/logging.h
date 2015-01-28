9e4a4b24202ce4a3bb3eed5e77dcccec131b9319
Ignas Daukšas <ignasd@gmail.com>
2015-01-28 20:24:38 +0200

    cleanup logging.h of tabs (aka y u do dis netbeans?)

--- src/logging.h
+++ src/logging.h
@@ -1,15 +1,15 @@
 #ifndef LOGGING_H
-#define	LOGGING_H
+#define LOGGING_H
 
-#ifdef	__cplusplus
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 void logging_info(const char *format, ...);
 void logging_error(const char *format, ...);
 
-#ifdef	__cplusplus
+#ifdef __cplusplus
 }
 #endif
 
-#endif	/* LOGGING_H */
+#endif /* LOGGING_H */
